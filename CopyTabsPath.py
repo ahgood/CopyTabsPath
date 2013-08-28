7d6a657de245a9ee4382a28a60ab2f6f3ad20827
Yang, Guojun <ahgood@gmail.com>
2013-08-29 03:07:03 +0800

    Update CopyTabsPath.py

--- CopyTabsPath.py
+++ CopyTabsPath.py
@@ -16,11 +16,9 @@ class CopyTabsPathCommand(sublime_plugin.WindowCommand):
             '''
             Close the tab if all of the following are true:
                 * File still exists
-                * File is not the current view in its window
             '''
             if (path
-                and os.path.exists(path) == True
-                and file.window() == None):                    
+                and os.path.exists(path) == True):                    
                     file_path += path + '\n'
         
         sublime.set_clipboard(file_path)
